e6e65b05c2c18dc44617e18fe95879420298423a
Lokendra Dangi <pachar.com@gmail.com>
2021-02-24 07:31:26 +0545

    BasicIO

---
+++ src/pkg2/TestIO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2;
+import pkg1.BasicIO;
+/**
+ *
+ * @author admin
+ */
+public class TestIO {
+    public static void main(String []args){
+        BasicIO io = new BasicIO();
+        io.print("Enter your id : ");
+    }
+}
